98d1449cf0dc8083d9dce589209817e0b9da231e
Svarshick <sablewithable@gmail.com>
2025-06-03 14:36:49 +0300

    Clear names, add FrontSetConfig

--- Diploma Speedrun/Assets/Scripts/FrontConfig.cs
+++ Diploma Speedrun/Assets/Scripts/FrontConfig.cs
@@ -4,44 +4,43 @@ using AYellowpaper.SerializedCollections;
 using UnityEngine;
 using DefaultNamespace;
 
-[Serializable]
-public class Choice
+namespace DefaultNamespace
 {
-    [SerializeField] public String text;
+    [Serializable]
+    public class Choice
+    {
+        [SerializeField] public String text;
 
-    [SerializedDictionary("Tag", "Value")]
-    [SerializeField]
-    public SerializedDictionary<FrontTag, bool> requirements;
+        [SerializedDictionary("Tag", "Value")] [SerializeField]
+        public SerializedDictionary<FrontTag, bool> requirements;
 
-    [SerializedDictionary("Tag", "Value")] 
-    [SerializeField]
-    public SerializedDictionary<FrontTag, bool> effect;
-}
-
-
-[Serializable]
-public class StageConfig
-{
-    [SerializeField] public String text;
-    [SerializeField] public List<Choice> choices;
-}
+        [SerializedDictionary("Tag", "Value")] [SerializeField]
+        public SerializedDictionary<FrontTag, bool> effect;
+    }
 
 
-[CreateAssetMenu(fileName = "FrontConfig", menuName = "Scriptable Objects/FrontConfig")]
-public class FronConfig : ScriptableObject
-{
-    [SerializeField] public String name;
-    [SerializeField] public int day;
+    [Serializable]
+    public class StageConfig
+    {
+        [SerializeField] public String text;
+        [SerializeField] public List<Choice> choices;
+    }
 
-    [SerializedDictionary("Tag", "Value")]
-    [SerializeField]
-    public SerializedDictionary<FrontTag, bool> requirements;
 
-    [SerializeField] public Location location;
+    [CreateAssetMenu(fileName = "FrontConfig", menuName = "Scriptable Objects/FrontConfig")]
+    public class FrontConfig : ScriptableObject
+    {
+        [SerializeField] public String name;
+        [SerializeField] public int day;
 
-    [SerializeField] public List<StageConfig> stages;
-    
-    [SerializedDictionary("Tag", "Value")]
-    [SerializeField]
-    public SerializedDictionary<FrontTag, bool> fiasco;
+        [SerializedDictionary("Tag", "Value")] [SerializeField]
+        public SerializedDictionary<FrontTag, bool> requirements;
+
+        [SerializeField] public Location location;
+
+        [SerializeField] public List<StageConfig> stages;
+
+        [SerializedDictionary("Tag", "Value")] [SerializeField]
+        public SerializedDictionary<FrontTag, bool> fiasco;
+    }
 }
---
+++ Diploma Speedrun/Assets/Scripts/FrontSetConfig.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+using DefaultNamespace;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "FrontSetConfig", menuName = "Scriptable Objects/FrontSetConfig")]
+    public class FrontSetConfig : ScriptableObject
+    {
+        [SerializeField] public List<FrontConfig> fronts;
+    }
+}
